df777397121167752a064a78c2f4598138516fbd
suyeon <hanyouwon1234@gmail.com>
2026-05-16 23:28:01 +0900

    hello

---
+++ src/main/java/com/sws/javaclass/JavaClassApplication.java
@@ -0,0 +1,13 @@
+package com.sws.javaclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sws/javaclass/JavaClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sws.javaclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
